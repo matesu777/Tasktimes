7e4f6e16fa0aef53e38d53c9ca5d0a6892ffe989
Maateuss77 <programadormateusa@gmail.com>
2026-02-07 22:35:10 -0300

    Update

--- src/modules/routes/route_task.ts
+++ src/modules/routes/route_task.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import type { FastifyInstance } from "fastify";
 import { TaskController } from "../controllers/task_controller";
 
 export const TaskRoute = async (app: FastifyInstance) => {
